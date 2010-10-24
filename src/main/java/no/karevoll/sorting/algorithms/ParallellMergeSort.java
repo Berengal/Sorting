db97fa3c37ef12c3d61a592591a2abbe3301a8da
Sjur Gjøstein Karevoll <sjurberengal@gmail.com>
2010-10-24 19:45:48 +0000

    Added SuperParallellMergeSort
    --HG--
    extra : convert_revision : svn%3A37d465e0-8b08-40c9-9636-11f50a1bb088/sorting/trunk%4017

--- src/main/java/no/karevoll/sorting/algorithms/ParallellMergeSort.java
+++ src/main/java/no/karevoll/sorting/algorithms/ParallellMergeSort.java
@@ -9,126 +9,112 @@ import no.karevoll.sorting.App;
 import no.karevoll.sorting.SortingAlgorithm;
 import no.karevoll.sorting.memory.MemoryArray;
 import no.karevoll.sorting.memory.MemoryManager;
+import no.karevoll.sorting.parallell.Counter;
 
 public class ParallellMergeSort implements SortingAlgorithm {
 
-    Executor executor;
-    MemoryArray input, scratchMemory;
-    Object done;
+  Executor executor;
+  MemoryArray input, scratchMemory;
+  Object done;
+  @Override
+  public void sort(MemoryArray input, MemoryManager memoryManager) {
+    this.input = input;
+    scratchMemory = memoryManager.allocate(input.getSize(),
+            "Scratch memory");
+    executor = new ThreadPoolExecutor(App.THREAD_POOL_SIZE,
+            App.THREAD_POOL_SIZE, 0, TimeUnit.SECONDS,
+            new LinkedBlockingQueue<Runnable>());
+    done = new Object();
+    executor.execute(new Merger(0, input.getSize(), new Counter(1),
+            new Runnable() {
+
+              @Override
+              public void run() {
+                synchronized (done) {
+                  done.notifyAll();
+                }
+              }
+
+            }));
+    synchronized (done) {
+      try {
+        done.wait();
+      } catch (InterruptedException ex) {
+        ex.printStackTrace();
+      }
+    }
+	memoryManager.free(scratchMemory);
+  }
+
+  class Merger implements Runnable {
+
+    int start, end, middle;
+    Counter counter;
+    Runnable parentMerge;
+    public Merger(int start, int end, Counter counter, Runnable parentMerge) {
+      this.start = start;
+      this.end = end;
+      middle = (start + end) / 2;
+      this.counter = counter;
+      this.parentMerge = parentMerge;
+    }
 
     @Override
-    public void sort(MemoryArray input, MemoryManager memoryManager) {
-	this.input = input;
-	scratchMemory = memoryManager.allocate(input.getSize(),
-		"Scratch memory");
-	executor = new ThreadPoolExecutor(App.THREAD_POOL_SIZE,
-		App.THREAD_POOL_SIZE, 0, TimeUnit.SECONDS,
-		new LinkedBlockingQueue<Runnable>());
-	done = new Object();
-	executor.execute(new Merger(0, input.getSize(), new Counter(1),
-		new Runnable() {
+    public void run() {
+      if (start >= end - 1) {
+        if (counter.dec()) {
+          parentMerge.run();
+        }
+        return;
+      }
+      Counter childCounter = new Counter(2);
+      Runnable childrenDone = new Runnable() {
 
-		    @Override
-		    public void run() {
-			synchronized (done) {
-			    done.notifyAll();
-			}
-		    }
+        @Override
+        public void run() {
+          merge();
+          if (counter.dec()) {
+            executor.execute(parentMerge);
+          }
+        }
 
-		}));
-	synchronized (done) {
-	    try {
-		done.wait();
-	    } catch (InterruptedException ex) {
-		ex.printStackTrace();
-	    }
-	}
-	memoryManager.free(scratchMemory);
+      };
+
+      Merger leftChild = new Merger(start, middle, childCounter, childrenDone);
+      Merger rightChild = new Merger(middle, end, childCounter, childrenDone);
+      executor.execute(leftChild);
+      executor.execute(rightChild);
     }
 
-    class Merger implements Runnable {
+    private void merge() {
 
-	int start, end, middle;
-	Counter counter;
-	Runnable parentMerge;
+      int i = start, j = middle;
 
-	public Merger(int start, int end, Counter counter, Runnable parentMerge) {
-	    this.start = start;
-	    this.end = end;
-	    middle = (start + end) / 2;
-	    this.counter = counter;
-	    this.parentMerge = parentMerge;
-	}
+      while (i < middle && j < end) {
+        if (input.read(i).compareTo((input.read(j))) < 0) {
+          scratchMemory.insert(input.remove(i), i + j - middle);
+          i++;
+        } else {
+          scratchMemory.insert(input.remove(j), i + j - middle);
+          j++;
+        }
+      }
 
-	@Override
-	public void run() {
-	    if (start >= end - 1) {
-		if (counter.dec()) {
-		    parentMerge.run();
-		}
-		return;
-	    }
-	    Counter childCounter = new Counter(2);
-	    Runnable magic = new Runnable() {
+      while (i < middle) {
+        scratchMemory.insert(input.remove(i), i + j - middle);
+        i++;
+      }
+      while (j < end) {
+        scratchMemory.insert(input.remove(j), i + j - middle);
+        j++;
+      }
 
-		@Override
-		public void run() {
-		    merge();
-		    if (counter.dec()) {
-			parentMerge.run();
-		    }
-		}
-
-	    };
-
-	    Merger leftChild = new Merger(start, middle, childCounter, magic);
-	    Merger rightChild = new Merger(middle, end, childCounter, magic);
-	    executor.execute(leftChild);
-	    executor.execute(rightChild);
-	}
-
-	private void merge() {
-
-	    int i = start, j = middle;
-
-	    while (i < middle && j < end) {
-		if (input.read(i).compareTo((input.read(j))) < 0) {
-		    scratchMemory.insert(input.remove(i), i + j - middle);
-		    i++;
-		} else {
-		    scratchMemory.insert(input.remove(j), i + j - middle);
-		    j++;
-		}
-	    }
-
-	    while (i < middle) {
-		scratchMemory.insert(input.remove(i), i + j - middle);
-		i++;
-	    }
-	    while (j < end) {
-		scratchMemory.insert(input.remove(j), i + j - middle);
-		j++;
-	    }
-
-	    for (int index = start; index < end; index++) {
-		input.insert(scratchMemory.remove(index), index);
-	    }
-	}
-
+      for (int index = start; index < end; index++) {
+        input.insert(scratchMemory.remove(index), index);
+      }
     }
 
-    class Counter {
+  }
 
-	private int count;
 
-	public Counter(int count) {
-	    this.count = count;
-	}
-
-	synchronized boolean dec() {
-	    count--;
-	    return count == 0;
-	}
-
-    }
 }
