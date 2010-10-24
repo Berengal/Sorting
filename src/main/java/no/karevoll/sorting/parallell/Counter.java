66fe26eb227872c9cfb9d23ff4d14f10e33b1c2d
Sjur Gjøstein Karevoll <sjurberengal@gmail.com>
2010-10-24 19:59:54 +0000

    Added Counter utility class
    --HG--
    extra : convert_revision : svn%3A37d465e0-8b08-40c9-9636-11f50a1bb088/sorting/trunk%4018

---
+++ src/main/java/no/karevoll/sorting/parallell/Counter.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package no.karevoll.sorting.parallell;
+
+/**
+ *
+ * @author berengal
+ */
+public class Counter {
+
+  private int count;
+  public Counter(int count) {
+    this.count = count;
+  }
+
+  public synchronized boolean dec() {
+    count--;
+    notifyAll();
+    return count == 0;
+  }
+
+  public synchronized void waitZero() {
+    while (count > 0) {
+      try {
+        wait();
+      } catch (InterruptedException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+    }
+  }
+
+}
